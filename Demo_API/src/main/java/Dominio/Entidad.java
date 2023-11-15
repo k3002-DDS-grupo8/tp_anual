d48781f28c06970a9aa2f1ac30722f4ab346c1c6
matikalo <44058368+matikalo@users.noreply.github.com>
2023-11-14 21:11:46 -0300

    agrego getter y setter en entidad

--- Demo_API/src/main/java/Dominio/Entidad.java
+++ Demo_API/src/main/java/Dominio/Entidad.java
@@ -9,10 +9,43 @@ public class Entidad {
         this.descripcion = descripcion;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
     private long id;
     private String nombre;
     private String email;
     private String descripcion;
+
 
 
 }

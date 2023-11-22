3e64164475553eeb01c8839fe20732a90b2f9e96
AbrilCaruso <102760575+AbrilCaruso@users.noreply.github.com>
2023-11-22 15:25:50 -0300

    Update Main.java

--- API_CP/src/main/java/Main.java
+++ API_CP/src/main/java/Main.java
@@ -11,20 +11,7 @@ import org.quartz.*;
 
 public class Main {
     public static void main(String[] args) throws SchedulerException {
-        //QUARTZ SCHEDULER
-        SchedulerFactory schedulerFactory = new org.quartz.impl.StdSchedulerFactory();
-        Scheduler scheduler = schedulerFactory.getScheduler();
-        scheduler.start();
-        Trigger trigger = TriggerBuilder.newTrigger()
-                .withIdentity("trigger1", "group1")
-                //.withSchedule(CronScheduleBuilder.cronSchedule("0 0 3 ? * SUN *")) // Todos los Domingos a las 03:00 am
-                .withSchedule(CronScheduleBuilder.cronSchedule("0 0/1 * 1/1 * ? *")) // Cada 1 minuto
-                .build();
-        JobDetail job = JobBuilder.newJob(MainJob.class)
-                .withIdentity("job1", "group1")
-                .build();
-        scheduler.scheduleJob(job, trigger);
-
+    
         //JAVALIN API
         var app = Javalin.create()
                 .get("/obtenerUsuarios", ctx -> {

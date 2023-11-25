5b3c940943357e93f1f46497e6fdfa3bc6a161c4
AbrilCaruso <102760575+AbrilCaruso@users.noreply.github.com>
2023-11-25 20:10:08 -0300

    Update GetIncidenteIdComunidadCerradoHandler.java

--- Cliente_Liviano/API_CL/src/main/java/Presentacion/GetIncidenteIdComunidadCerradoHandler.java
+++ Cliente_Liviano/API_CL/src/main/java/Presentacion/GetIncidenteIdComunidadCerradoHandler.java
@@ -1,4 +1,8 @@
 package Presentacion;
 
-public class GetIncidenteIdComunidadCerradoHandler {
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+
+public class GetIncidenteIdComunidadCerradoHandler implements Handler  {
 }

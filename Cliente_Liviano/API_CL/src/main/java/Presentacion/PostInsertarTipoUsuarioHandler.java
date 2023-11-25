8a7a2eef4e42e164ad05f512af8593388f99c1b2
Mguada <89551678+MGuada@users.noreply.github.com>
2023-11-25 20:13:18 -0300

    Update PostInsertarTipoUsuarioHandler.java

--- Cliente_Liviano/API_CL/src/main/java/Presentacion/PostInsertarTipoUsuarioHandler.java
+++ Cliente_Liviano/API_CL/src/main/java/Presentacion/PostInsertarTipoUsuarioHandler.java
@@ -1,4 +1,8 @@
 package Presentacion;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
 
-public class PostInsertarTipoUsuarioHandler {
+
+public class PostInsertarTipoUsuarioHandler implements Handler {
 }

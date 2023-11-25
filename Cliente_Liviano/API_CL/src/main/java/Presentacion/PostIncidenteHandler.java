3e6393123ceca3757d83d769d87bf022914e5b84
AbrilCaruso <102760575+AbrilCaruso@users.noreply.github.com>
2023-11-25 20:14:44 -0300

    Update PostIncidenteHandler.java

--- Cliente_Liviano/API_CL/src/main/java/Presentacion/PostIncidenteHandler.java
+++ Cliente_Liviano/API_CL/src/main/java/Presentacion/PostIncidenteHandler.java
@@ -1,8 +1,8 @@
 package Presentacion;
+
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import org.jetbrains.annotations.NotNull;
-
 
 public class PostIncidenteHandler implements Handler{
 }

6ff5cb4146066347d88dcff5558aeb0edb6b30d8
FrancoModesto <franco.modesto@hotmail.com>
2023-12-10 07:09:05 -0300

    denada 2

---
+++ Cliente_Liviano/API_CL/src/main/java/Presentacion/LoginHandler.java
@@ -0,0 +1,13 @@
+package Presentacion;
+
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+
+public class LoginHandler implements Handler {
+
+   @Override
+    public void handle(@NotNull Context context) throws Exception {
+            falta hacer
+    }
+}
---
+++ Cliente_Liviano/API_CL/src/main/java/Presentacion/PostCargaMasivaEntidadesHandler.java
@@ -0,0 +1,12 @@
+package Presentacion;
+
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+
+public class PostCargaMasivaEntidadesHandler implements Handler  {
+  @Override
+    public void handle(@NotNull Context context) throws Exception {
+
+    }
+}

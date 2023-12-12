67ab1a586d240d4a8a135a0f60cce89ad52edb47
AbrilCaruso <102760575+AbrilCaruso@users.noreply.github.com>
2023-12-12 20:02:17 -0300

    Update Organizacion.java

--- Cliente_Liviano/API_CL/src/main/java/Dominio/entidad/Organizacion.java
+++ Cliente_Liviano/API_CL/src/main/java/Dominio/entidad/Organizacion.java
@@ -1,15 +1,17 @@
 package Dominio.entidad;
 import Dominio.comunidad.EEO;
 import Dominio.localizacion.Localizacion;
-
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.persistence.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
-@Entity
+
 public class Organizacion extends EntidadPrestadora {
-    @Enumerated
+    @JsonProperty("tipo")
     final TipoOrganizacion tipo;
 
+    @JsonCreator
     public Organizacion(String nombre, ArrayList<Establecimiento> listaEstablecimientos, EEO usuarioEEO , ArrayList<Localizacion> localizacion, TipoOrganizacion tipo) {
         super(nombre, listaEstablecimientos, localizacion, usuarioEEO);
         this.tipo = tipo;

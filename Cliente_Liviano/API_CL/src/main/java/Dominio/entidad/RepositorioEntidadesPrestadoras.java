0c4586beae7d80add812dc3c783d7a93b2c5e7e4
AbrilCaruso <102760575+AbrilCaruso@users.noreply.github.com>
2023-12-12 20:02:58 -0300

    Update RepositorioEntidadesPrestadoras.java

--- Cliente_Liviano/API_CL/src/main/java/Dominio/entidad/RepositorioEntidadesPrestadoras.java
+++ Cliente_Liviano/API_CL/src/main/java/Dominio/entidad/RepositorioEntidadesPrestadoras.java
@@ -1,13 +1,15 @@
 package Dominio.entidad;
 
 import java.util.ArrayList;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 ///////////////////
 //CHEQUEAR ESTA CLASE
 //////////////////
 
-
 public class RepositorioEntidadesPrestadoras {
+    @JsonProperty("entidades")
     public ArrayList<EntidadPrestadora> entidades;
 
     public ArrayList<EntidadPrestadora> getEntidades() {
